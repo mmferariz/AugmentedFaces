ed2150114fbbcca085433a6b5cc2caccc0089140
Dev <dev@onikom.com.mx>
2023-10-11 02:02:50 -0600

    fix: session fix: add and remove texture fix: test

--- ArCoreLib/src/main/java/com/mmferariz/arcorelib/components/ArCoreFragment.kt
+++ ArCoreLib/src/main/java/com/mmferariz/arcorelib/components/ArCoreFragment.kt
@@ -2,9 +2,12 @@ package com.mmferariz.arcorelib.components
 
 import android.Manifest
 import android.app.Activity
+import android.app.Application
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Bundle
 import android.util.AttributeSet
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.google.ar.core.ArCoreApk
@@ -14,8 +17,7 @@ import com.google.ar.core.CameraConfigFilter
 import com.google.ar.core.Config
 import com.google.ar.core.Session
 import com.google.ar.core.TrackingState
-import com.google.ar.core.exceptions.UnavailableException
-import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException
+import com.google.ar.core.exceptions.CameraNotAvailableException
 import com.google.ar.sceneform.ArSceneView
 import com.google.ar.sceneform.rendering.Renderable
 import com.google.ar.sceneform.rendering.Texture
@@ -25,7 +27,7 @@ import java.lang.Exception
 import java.util.EnumSet
 import java.util.HashMap
 
-class ArCoreFragment @JvmOverloads constructor(
+class CustomArView @JvmOverloads constructor(
     context: Context,
     activity: Activity,
     attrs: AttributeSet? = null,
@@ -35,46 +37,50 @@ class ArCoreFragment @JvmOverloads constructor(
     private var isAdded = false
     private var isFrontCamera = true
     private val faceNodeMap = HashMap<AugmentedFace, AugmentedFaceNode>()
+    private var activity: Activity
+    private lateinit var activityLifecycleCallbacks: Application.ActivityLifecycleCallbacks
 
-        init {
-            if(initSession(activity)){
-                Texture.builder()
-                    .setSource(context, R.drawable.fox_face_mesh_texture)
-                    .build()
-                    .thenAccept { textureModel -> texture = textureModel }
-                    .exceptionally {
-                        Toast.makeText(context, "Cannot load texture", Toast.LENGTH_SHORT).show()
-                        null
+    init {
+            this.activity = activity
+            layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
+            Texture.builder()
+                .setSource(context, R.drawable.fox_face_mesh_texture)
+                .build()
+                .thenAccept { textureModel -> texture = textureModel }
+                .exceptionally {
+                    Toast.makeText(context, "Cannot load texture", Toast.LENGTH_SHORT).show()
+                    null
+                }
+
+            cameraStreamRenderPriority = Renderable.RENDER_PRIORITY_FIRST
+            scene?.addOnUpdateListener {
+                if (texture == null) return@addOnUpdateListener
+
+                val frame = arFrame ?: return@addOnUpdateListener
+                val augmentedFaces = session?.getAllTrackables(AugmentedFace::class.java) ?: listOf()
+
+                for (augmentedFace in augmentedFaces) {
+                    if (!faceNodeMap.containsKey(augmentedFace)) {
+                        val faceNode = AugmentedFaceNode(augmentedFace)
+                        faceNode.setParent(scene)
+                        faceNode.faceMeshTexture = texture
+                        faceNodeMap[augmentedFace] = faceNode
                     }
+                }
 
-                cameraStreamRenderPriority = Renderable.RENDER_PRIORITY_FIRST
-                scene?.addOnUpdateListener {
-                    if (texture == null) return@addOnUpdateListener
-
-                    val frame = arFrame ?: return@addOnUpdateListener
-                    val augmentedFaces = frame.getUpdatedTrackables(AugmentedFace::class.java)
-
-                    for (augmentedFace in augmentedFaces) {
-                        if (isAdded) return@addOnUpdateListener
-
-                        val augmentedFaceNode = AugmentedFaceNode(augmentedFace).apply {
-                            setParent(scene)
-                            setFaceMeshTexture(texture)
-                        }
-                        faceNodeMap[augmentedFace] = augmentedFaceNode
-                        isAdded = true
-
-                        val iterator = faceNodeMap.entries.iterator()
-                        while (iterator.hasNext()) {
-                            val entry = iterator.next()
-                            if (entry.key.trackingState == TrackingState.STOPPED) {
-                                entry.value.setParent(null)
-                                iterator.remove()
-                            }
-                        }
+                val iter = faceNodeMap.iterator()
+                while (iter.hasNext()) {
+                    val entry = iter.next()
+                    val face = entry.key
+                    if (face.trackingState == TrackingState.STOPPED) {
+                        val faceNode = entry.value
+                        faceNode.setParent(null)
+                        iter.remove()
                     }
                 }
             }
+
+            setupLifeCycle(context)
         }
 
     fun createArSession(activity: Activity, isFrontCamera: Boolean): Session? {
@@ -123,11 +129,11 @@ class ArCoreFragment @JvmOverloads constructor(
         return ContextCompat.checkSelfPermission(activity, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
     }
 
-    private fun initSession(activity: Activity): Boolean{
+    private fun initSession(): Boolean{
         if (session == null) {
             try {
                 val session = createArSession(activity, isFrontCamera)
-                if (session != null) {
+                return if (session != null) {
                     val config = Config(session)
                     if (isFrontCamera) {
                         config.augmentedFaceMode = Config.AugmentedFaceMode.MESH3D
@@ -136,9 +142,10 @@ class ArCoreFragment @JvmOverloads constructor(
                     config.focusMode = Config.FocusMode.AUTO;
                     session.configure(config)
                     setupSession(session)
-                    return true
+                    true
+                } else {
+                    false
                 }
-                return false
             } catch (ex: Exception) {
                 return false
             }
@@ -147,4 +154,58 @@ class ArCoreFragment @JvmOverloads constructor(
         }
     }
 
+    private fun setupLifeCycle(context: Context) {
+        activityLifecycleCallbacks = object : Application.ActivityLifecycleCallbacks {
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+//                maybeEnableArButton()
+            }
+
+            override fun onActivityStarted(activity: Activity) {
+            }
+
+            override fun onActivityResumed(activity: Activity) {
+                onResume()
+            }
+
+            override fun onActivityPaused(activity: Activity) {
+                onPause()
+            }
+
+            override fun onActivityStopped(activity: Activity) {
+//                onPause()
+            }
+
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+
+            override fun onActivityDestroyed(activity: Activity) {
+//                onDestroy()
+//                dispose()
+            }
+        }
+
+        activity.application.registerActivityLifecycleCallbacks(this.activityLifecycleCallbacks)
+    }
+
+    fun onResume() {
+        // request camera permission if not already requested
+        if (hasCameraPermission(activity)) {
+            //ArCoreUtils.requestCameraPermission(activity, RC_PERMISSIONS)
+        }
+
+        initSession()
+
+        try {
+            resume()
+        } catch (ex: CameraNotAvailableException) {
+            activity.finish()
+            return
+        }
+
+    }
+
+    fun onPause() {
+        // if (this != null) {
+        //     arSceneView?.pause()
+        // }
+    }
 }
--- app/src/main/java/com/example/arsnapchat/MainActivity.kt
+++ app/src/main/java/com/example/arsnapchat/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.arsnapchat
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.mmferariz.arcorelib.components.CustomArView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        findViewById<LinearLayout>(R.id.layout).addView(CustomArView(this, this))
     }
 }
